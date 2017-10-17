6db6af9859238f16f3688b8248fb61b8c7b8dfce
Luke Green <elukuent@gmail.com>
2017-10-16 21:42:35 -0700

    getting old app dir ported over

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,30 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+$(document).ready(function(){
+
+  /*Change active class for menu items*/
+
+  $('.nav li').on('click', function(){
+    $('.nav li').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  /*Fade in nav background*/
+
+  $(window).scroll(function(){
+    if ($(window).scrollTop() > 100 ) {
+      $('.nav-bg-fade').fadeIn(500);
+    } else {
+      $('.nav-bg-fade').fadeOut(500);
+ 	  };
+  });
+
+  /*Music tile interactivity*/
+
+  /*$('.album').hover(function(){
+    var elid = $(this).attr('id');
+    $('#'+elid).css('background','url(../images/Chika-pics-final/kokorohane-bw.jpg) no-repeat center');
+    console.log(elid);
+  }, function(){
+    console.log("whoa");
+  });*/
+
+})
